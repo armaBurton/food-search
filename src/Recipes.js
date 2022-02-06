804b1a226d4dde9a2ccad01ab59d51bec7bb595b
armaBurton <armanarma@armaburton.com>
2022-02-05 16:06:46 -0800

    finished-ish app

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,10 +1,18 @@
 export default function Recipes(props){
+  console.log(props.recipe);
   const cal = Math.floor(props.recipe.calories);
   return (
-    <div>
+    <div className='recipe-card'>
       <h1>{props.recipe.label}</h1>
       <p>Calories: { cal }</p>
       <img src={props.recipe.image} alt={props.recipe.label} />
+      <ul className="">
+        {
+          props.recipe.ingredients.map((ingredient, i) => (<li key={ingredient + i} >
+            {ingredient.text}
+          </li>))
+        }
+      </ul>
     </div>
   );
 }
